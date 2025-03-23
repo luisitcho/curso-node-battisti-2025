0fd54d3fb102cefd3268f8775812eed7d12f55fb
luisitcho <luishenrisc1@gmail.com>
2025-03-22 22:50:43 -0300

    curso node - aula 4 - (import export)

---
+++ 2_fundamentos/2_import_export/main.mjs
@@ -0,0 +1,4 @@
+import { soma, subtracao } from './modulo.mjs';
+
+console.log(soma(5, 5)); // Outputs: 10
+console.log(subtracao(5, 1)); // Outputs: 4
---
+++ 2_fundamentos/2_import_export/modulo.mjs
@@ -0,0 +1,6 @@
+export function soma(a, b) {
+    return a + b;
+}
+export function subtracao(a, b) {
+    return a - b;
+}
