9c4ea6797bb56b8abc868d34d159c6f95a641c5d
luisitcho <luishenrisc1@gmail.com>
2025-03-22 21:56:51 -0300

    curso node - aula 2 (modulos)

--- 1_fundamentos/2_modulos/main.js
+++ 1_fundamentos/2_modulos/main.js
@@ -1,11 +1,13 @@
-const fs = require('fs');
+    const fs = require('fs');
+    const path = require('path');
+    const filePath = path.join(__dirname, 'file.txt');
 
-fs.readFile('file.txt', 'utf-8', function (err, data) {
+    fs.readFile(filePath, 'utf-8', function (err, data) {
 
-    if (err) {
-        console.error('Error reading file:', err);
-        return;
-    }
-    console.log(data);
+        if (err) {
+            console.error('Error reading file:', err);
+            return;
+        }
+        console.log(data);
 
-});
+    });
