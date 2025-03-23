8ea836536e40c6192b2bb8a2e3b1510fb12c6b9b
luisitcho <luishenrisc1@gmail.com>
2025-03-22 22:12:14 -0300

    curso node - aula 3 - (modulo interno)

---
+++ 2_fundamentos/1_modulo_interno/main.js
@@ -0,0 +1,6 @@
+const modulo = require('./modulo');
+const soma = modulo.soma;
+const subtracao = modulo.subtracao;
+
+console.log(soma(5, 3)); // Outputs: 8
+console.log(subtracao(5, 3)); // Outputs: 2
---
+++ 2_fundamentos/1_modulo_interno/modulo.js
@@ -0,0 +1,8 @@
+module.exports = {
+    soma(a, b) {
+        return a + b;
+    },
+    subtracao(a, b) {
+        return a - b;
+    }
+}
